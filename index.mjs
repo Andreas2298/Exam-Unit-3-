6cda1675f01479ef8d29e4c033a0f8d7cce5ec73
Andreas2298 <andre-as13@hotmail.no>
2025-03-19 15:36:32 +0100

    Set up solveChallenges function to handle input for the first challenge.

--- index.mjs
+++ index.mjs
@@ -1,1 +1,10 @@
+console.log("starting the UiA Alchehmy Challenge");
 
+function solveChallenges() {
+  console.log("First challenge input:", input);
+
+  return "first_solution";
+}
+
+const solution = solveChallenges("first challenge_data");
+console.log("First Solution:", solution);
