1f617d22ddc244af87ceedaa0c1bd659949ca676
Andreas2298 <andre-as13@hotmail.no>
2025-03-21 15:17:32 +0100

    Implemented first API request to fetch the first challenge using node-fetch.

--- index.mjs
+++ index.mjs
@@ -1,10 +1,29 @@
+import fetch from "node-fetch";
+
+const PLAYER_NAME = "andreasn@uia.no";
+const BASE_URL = "https://bit.ly/UiA_Alchemy_25";
+
 console.log("starting the UiA Alchehmy Challenge");
 
-function solveChallenges() {
-  console.log("First challenge input:", input);
+async function startChallenge() {
+  try {
+    const response = await fetch(`${BASE_URL}/start?player=${PLAYER_NAME}`);
 
-  return "first_solution";
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log("The first challenge is received");
+    console.log(data);
+
+    return data;
+  } catch (error) {
+    console.error(
+      "There was error fetching the challenge. Please try again:",
+      error.message
+    );
+  }
 }
 
-const solution = solveChallenges("first challenge_data");
-console.log("First Solution:", solution);
+startChallenge();
