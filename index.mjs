db436a55ca750d64850f273156332c6189408d0e
Andreas2298 <andre-as13@hotmail.no>
2025-03-21 15:35:36 +0100

    API request was set up to fetch the first challenge from the endpoint.

--- index.mjs
+++ index.mjs
@@ -1,23 +1,34 @@
 import fetch from "node-fetch";
 
 const PLAYER_NAME = "andreasn@uia.no";
-const BASE_URL = "https://bit.ly/UiA_Alchemy_25";
+const BASE_URL = "https://alchemy-kd0l.onrender.com";
 
 console.log("starting the UiA Alchehmy Challenge");
 
 async function startChallenge() {
   try {
-    const response = await fetch(`${BASE_URL}/start?player=${PLAYER_NAME}`);
+    const response = await fetch(`${BASE_URL}/start?player=${PLAYER_NAME}`, {
+      redirect: "follow",
+    });
 
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
-    const data = await response.json();
-    console.log("The first challenge is received");
-    console.log(data);
+    const contentType = response.headers.get("content-type");
 
-    return data;
+    if (contentType && contentType.includes("application/json")) {
+      const data = await response.json();
+      console.log("The first challenge is received");
+      console.log(data);
+
+      return data;
+    } else {
+      console.log("The response is not json. Try again");
+      const text = await response.text();
+
+      console.log(text);
+    }
   } catch (error) {
     console.error(
       "There was error fetching the challenge. Please try again:",
